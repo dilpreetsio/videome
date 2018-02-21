11e758d689df609974591c67d256cdf578bc45c7
geekyd <singhsince94@gmail.com>
2018-02-21 13:21:48 +0530

    Basic video from images

--- video.py
+++ video.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
-cmd = "ffmpeg --version";
+cmd = "ffmpeg -r 1/4 -f image2 -s 1920x1080 -i images/%d.png -vframes 1000 -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4";
 
-returned = os.system(cmd);
+images = [];
+images = os.listdir("./images");
+sound = os.listdir("./sound");
+
+os.system(cmd);
+images.sort();
