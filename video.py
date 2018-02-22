f8bf2a64801cd5fcc82bb54df3b9cf121716cc28
geekyd <singhsince94@gmail.com>
2018-02-22 10:15:38 +0530

    Add music to videos

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-cmd = "ffmpeg -r 1/4 -f image2 -s 1920x1080 -i images/%d.png -vframes 1000 -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4";
+cmd = "ffmpeg -r 1/9 -f image2 -s 1920x1080 -i images/%d.png -i sound/sound.mp3 -vframes 1000 -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4";
 
 images = [];
 images = os.listdir("./images");
